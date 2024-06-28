e23b2571b15d2d7bc2188b994092e7683ec9e583
Julieth Giraldo <156958433+JuliethGS0101@users.noreply.github.com>
2024-06-27 19:02:06 -0500

    datos anime

--- desafio/src/main/java/com/aluracursos/desafio/model/DatosAnime.java
+++ desafio/src/main/java/com/aluracursos/desafio/model/DatosAnime.java
@@ -1,7 +1,12 @@
 package com.aluracursos.desafio.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public record DatosAnime(
-        String titulo,
+        @JsonAlias("title") String titulo,
+        @JsonAlias ("episodes") Integer numeroEpisodio,
+        @JsonAlias ("status") String estado,
+        @JsonAlias ("rating") String clasificacion
 
 ) {
 }
